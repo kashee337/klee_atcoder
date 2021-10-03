8bb3d6020621b4de14170e4678292402cf09e8d8
klee <k.r.okayama@gmail.com>
2021-10-03 16:48:17 +0900

    fix bit

--- lib/cpp/bit.cpp
+++ lib/cpp/bit.cpp
@@ -5,21 +5,23 @@ template <typename T> struct BIT {
     const T n;
 
   public:
-    BIT(int _n) : dat(_n + 1, 0), n(_n) {}
-    T sum(int i) {
-        T s = 0;
+    BIT(int _n) : dat(_n + 5, 0), n(_n) {}
+    T sum(int i, int j) {
+        if (i > j) return 0;
+        return _sum(j) - _sum(i - 1);
+    }
+
+    T _sum(int i) {
+        i++;
+        T res = 0;
         while (i > 0) {
-            s += dat[i];
+            res += dat[i];
             i -= i & (-i);
         }
-        return s;
-    }
-    T sum(int i, int j) {
-        T val_i = sum(i - 1);
-        T val_j = sum(j);
-        return val_j - val_i;
+        return res;
     }
     void add(int i, T x) {
+        i++;
         while (i <= n) {
             dat[i] += x;
             i += i & (-i);
